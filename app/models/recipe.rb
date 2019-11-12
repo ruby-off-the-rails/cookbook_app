80f5ef1e513d7a84a94e7ac10799fdb87a451042
brian <riceb53@gmail.com>
2019-11-11 18:41:29 -0600

    add model methods for ingredients, directions and friendly_created_at

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,15 @@
 class Recipe < ApplicationRecord
+  # works
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  # doesn't
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%A, %b %d")
+  end
 end
