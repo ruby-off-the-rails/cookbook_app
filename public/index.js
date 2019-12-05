ae1c682a26b548d289856a322cd13fd82b9fb1ac
brian <riceb53@gmail.com>
2019-12-04 21:30:03 -0600

    add async code

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
+/* global axios */
+
 console.log('hello there');
+
+
+// HTTP.get("localhost:3000/api/recipes")
+
+axios.get("/api/recipes").then(function(response) {
+  console.log("i am on line 9")
+  console.log(response);
+});
+
+console.log("i am on line 12")
