17831a7730ccffcfdb8fbaad2c97458b45757441
brian <riceb53@gmail.com>
2019-12-08 10:14:52 -0600

    show data on HTML page using JavaScript

--- public/index.js
+++ public/index.js
@@ -6,8 +6,19 @@ console.log('hello there');
 // HTTP.get("localhost:3000/api/recipes")
 
 axios.get("/api/recipes").then(function(response) {
-  console.log("i am on line 9")
-  console.log(response);
+  console.log("i am on line 9");
+  console.log(response.data);
+  // find an element on the page
+  var message = document.querySelector('#message');
+  console.log(message);
+
+  // change that element on the page
+  message.innerHTML = response.data[5].title;
 });
 
-console.log("i am on line 12")
+console.log('new update');
+
+// show information from js in HTML
+
+
+
