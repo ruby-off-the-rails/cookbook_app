b8275c97f7a17609e2a55c4f7642f53977691f39
brian <riceb53@gmail.com>
2020-01-05 09:45:08 -0600

    add test for create action

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -15,4 +15,29 @@ RSpec.describe "Recipes", type: :request do
       expect(recipes.length).to eq(3)
     end
   end
+  describe "POST /recipes" do
+    it 'should create a new recipe in the database' do
+      user = User.new(email: "bob@bob.com", password: "password", name: "robert")
+      user.save
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        'HS256' # the encryption algorithm
+      )
+      post "/api/recipes", params: {
+        chef: "brian",
+        title: "hot chicken",
+        ingredients: "spices, chicken",
+        directions: "make the chicken hot",
+        prep_time: 20
+      }, headers: {"Authorization" => "Bearer #{jwt}"}
+
+      recipe = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(recipe['chef']).to eq("brian")
+    end
+  end
 end
