950a9fca992f43c2a04b746b6cf1152ac408d5ab
brian <riceb53@gmail.com>
2020-01-07 17:41:39 -0600

    remove non-api urls

--- config/routes.rb
+++ config/routes.rb
@@ -16,13 +16,13 @@ Rails.application.routes.draw do
     post "/sessions" => "sessions#create"
   end
 
-  get "/recipes" => "recipes#index"
-  get "/recipes/new" => "recipes#new"
-  post "/recipes" => "recipes#create"
-  get "/recipes/:id" => "recipes#show"
-  get "/recipes/:id/edit" => "recipes#edit"
-  patch "/recipes/:id" => "recipes#update"
-  delete "/recipes/:id" => "recipes#destroy"
+  # get "/recipes" => "recipes#index"
+  # get "/recipes/new" => "recipes#new"
+  # post "/recipes" => "recipes#create"
+  # get "/recipes/:id" => "recipes#show"
+  # get "/recipes/:id/edit" => "recipes#edit"
+  # patch "/recipes/:id" => "recipes#update"
+  # delete "/recipes/:id" => "recipes#destroy"
 end
 
 # RESTful
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "Recipes", type: :request do
           user_id: user.id, # the data to encode
           exp: 24.hours.from_now.to_i # the expiration time
         },
-        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "random", # the secret key
         'HS256' # the encryption algorithm
       )
       post "/api/recipes", params: {
